c2c7db89916197baa715674a40f448b490b09d21
Alvaro <alvaro.fernandez@iesnervion.es>
2021-03-05 19:59:39 +0100

    Implementado el metodo del link en el menu

--- src/menu/Menu.java
+++ src/menu/Menu.java
@@ -8,10 +8,20 @@ package menu;
  *
  */
 import java.util.Scanner;
-
 import clases.Buscador;
+import clases.Pagina;
+
+//TODO comentar metodos con javadoc
 
 public class Menu {
+	
+	static Buscador buscador= new Buscador();
+	
+	public static void io(String hiper)
+	{
+
+
+	}
 	final static String  Asteriscos="**********************************************";
 	/** Este metodo te muestra las lineas del menu que te pregunta
 	 * 
@@ -35,7 +45,7 @@ public class Menu {
 	public static void maneja() {
 		visualizador();
 		Scanner sc= new Scanner(System.in);
-		String opcion;
+		int opcion;
 		String url;
 		String descripcion;
 		int pageRank;
@@ -43,9 +53,9 @@ public class Menu {
 		do {
 			
 			System.out.println("Elige opcion.");
-			opcion=sc.next();
+			opcion=sc.nextInt();
 			switch (opcion) {
-			case "1": {
+			case 1: {
 				//TODO Llamar metodo "Dar alta una pagina nueva"
 				System.out.println("\n"+Asteriscos);
 				System.out.println("-¿Cual es la URL?");
@@ -56,19 +66,56 @@ public class Menu {
 				pageRank=sc.nextInt();
 				System.out.println("\n -Indique el listado de palabras clave");
 				palabrasClaves=sc.next();
-				String[] palabras= palabrasClaves.split("\s");
+				String[] palabras= palabrasClaves.split(" ");
 				System.out.println("\n -Pagina hacia la que enlaza (Si no enlaza a ninguna pagina, deje en blanco la respuesta):");
+				String hipervinculo=sc.next();
 				
+				 System.out.println("1.Ingresar sin hipervinculo");
+			     System.out.println("2.Ingresar con hipervinculo");
+
+			        Scanner teclado = new Scanner(System.in);
+
+			        int eleccion = teclado.nextInt();
+
+			        switch (eleccion) {
+
+			            case 1:
+			            	Pagina paginaAgregada=new Pagina(url,descripcion,pageRank,palabras);
+			            	buscador.agregarPagina(paginaAgregada);
+
+			                break;
+
+			            case 2:
+			            	
+					         if(buscador.comprobarPagina(hipervinculo)) 
+					         {
+					        	 
+					             Pagina paginaDevuelta = buscador.obtenerPagina(hipervinculo);
+					             Pagina paginaAniadida=new Pagina(url, descripcion, pageRank, paginaDevuelta, palabras);
+					             buscador.agregarPagina(paginaAniadida);
+					             
+					         }else 
+					         {
+
+					             System.out.println("no hay pagina asi");
+					             opcion=4;
+					         
+
+			                break;
+
+					         }
+			        }
+		         	         
 				//Metodos para  hipervinculo, luego llamamos al metodo para dar de alta.
 				System.out.println(Asteriscos);
 				break;
 			}
-			case "2": {
+			case 2: {
 				//TODO Llamar metodo "Buscar paginas por palabras claves"
 				System.out.println("El 2");
 				break;
 			}
-			case "3": {
+			case 3: {
 				System.out.println("Se esta cerrando el programa, pasa un buen dia.");
 				//boolean rendirse=true;
 				break;
@@ -77,7 +124,7 @@ public class Menu {
 				System.out.println("Ese valor no es valido.");
 				break;
 			}
-		}while (opcion!="3");//booal rendirse  Probar con while
-		
+		}while (opcion!=3);//booal rendirse  Probar con while
+		sc.close();
 	}
 }
