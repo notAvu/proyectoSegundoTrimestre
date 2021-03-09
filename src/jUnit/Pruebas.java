1484342c0ed07c1b5d0a6d768575ad919bc68095
Antonio <Antonio@DESKTOP-C1EEH4Q>
2021-03-09 20:43:05 +0100

    JUnit... https://miro.medium.com/max/1600/0*m4Xf74GU-uP9xkif.jpg

--- src/jUnit/Pruebas.java
+++ src/jUnit/Pruebas.java
@@ -2,13 +2,6 @@ package jUnit;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
-
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import clases.Buscador;
@@ -25,50 +18,68 @@ class Pruebas {
 
 	private String[] arrayEjemplo3= {"tienda","móviles"};
 	private Pagina ejemplo3=new Pagina("http://ejemplo3", "ejemplo3", 3, arrayEjemplo3);
-	
-	private String[] arrayEjemplo4= {"nada"};
-	private Pagina ejemplo4=new Pagina("http://ejemplo4", "ejemplo4", 4, arrayEjemplo4);
-	
-	//iniciallizacion de pagina para hacer otras pruebas
-	private Pagina prueba1;
 
-	private final ByteArrayOutputStream outContent=new ByteArrayOutputStream();
-	private final PrintStream originalOut=System.out;
+	Buscador buscador=new Buscador();
 
-	
-	//Reiniciar flujo
-	@BeforeEach
-	void reiniciarFlujo() {
-		/*String[] arrayPrueba1= {"pagina","prueba","tienda"};
-		prueba1=new Pagina("prueba", "página de prueba", 1, arrayPrueba1);*/
-		System.setOut(new PrintStream(outContent));
-	}
-	
-	@AfterEach
-	void reiniciarFlujo2() {
-		System.setOut(originalOut);
-	}
 
-	
-	//Test
+//Tests
+//Pagina
 	@Test
 	public void testSetHipervinculo() {
 		ejemplo3.setHipervinculo(ejemplo2);
 		assertEquals(3, ejemplo2.getPageRank());
 	}
-	
-	@Test
-	public void testGetCoincidencias() {
-		String[] array= {"tienda"};
-		//TODO llamar a metodo getCoincidencias de Pagina
-		assertEquals(3,);
+
+	@Test //Test en el que no coinciden palabras
+	public void testGetCoincidenciasCero() {
+		String[] array= {"ninguna"};
+		assertEquals(0,ejemplo1.getCoincidencias(array));
 	}
 
-	/*
-	@Test
-	public void testToString() {
-		prueba1.toString();
-		assertTrue(outContent.toString().contains("prueba"));
+	@Test //Test en el que coinciden algunas de todas
+	public void testGetCoincidenciasAlgunas() {
+		String[] array= {"tienda", "patinete", "moda"};
+		assertEquals(2,ejemplo1.getCoincidencias(array));
 	}
-	*/
+
+	@Test //Test en el que coinciden todas las palabras
+	public void testGetCoincidenciasTodas() {
+		assertEquals(3,ejemplo1.getCoincidencias(arrayEjemplo1));
+	}
+
+//Buscador
+	@Test //Test que comprueba que no se hacer JUnit bien
+	public void testAgregarPagina() {
+		String[] arrayprueba= {"palabra"};
+		buscador.agregarPagina(new Pagina("prueba.com", "prueba", 1, arrayprueba));
+		Object[] array= {"	prueba.com\nprueba"};
+		assertArrayEquals(array, buscador.getMemoria());
+	}
+
+	@Test //Test que comprueba que me hizo odiar JUnit
+	public void testOrdenarBusqueda() {
+		String[] palabras= {"tienda"};
+		Pagina[] resultado=buscador.ordenarBusqueda(palabras);
+		String[] array= {"asdf"};
+		assertArrayEquals(array, resultado);
+
+	}
+
+	@Test //Test que comprueba que voy a suspender el proximo examen
+	public void testObtenerPagina() {
+		String[] arrayprueba= {"palabra"};
+		buscador.agregarPagina(new Pagina("prueba.com", "prueba", 1, arrayprueba));
+		assertEquals("prueba.com prueba", buscador.obtenerPagina("prueba.com"));
+	}
+	
+	//Estas pruebas unitarias en JUnit y sus comentarios son responsabilidad de Antonio, espero que saquen una sonrisa en quien lo lea porque las lagrimas ya me las he usado yo todas :')
+	
+	
+	@Test //Test que comprueba que la pagina se ha añadido a la memoria
+	public void testComprobarPagina() {
+		String[] arrayprueba= {"palabra"};
+		buscador.agregarPagina(new Pagina("prueba.com", "prueba", 1, arrayprueba));
+		assertTrue(buscador.comprobarPagina("prueba.com"));
+	}
+	
 }
